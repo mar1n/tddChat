be4cd77ecf6cacfbf5d798c080e6f976f6ebde20
mar1n <30738216+mar1n@users.noreply.github.com>
2022-08-05 20:16:06 +0100

    test red: react router change route

--- front/src/tests/reactRouter.test.js
+++ front/src/tests/reactRouter.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
+import userEvent from "@testing-library/user-event";
+
 import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import App from "../App";
 
@@ -9,5 +11,15 @@ describe('Router', () => {
 
         const home = screen.getByText(/You are home/i);
         expect(home).toBeInTheDocument();
+     })
+     test('should first', () => { 
+        render(<App />, {wrapper: BrowserRouter});
+        const user = userEvent.setup();
+        const home = screen.getByText(/You are home/i);
+        expect(home).toBeInTheDocument();
+
+        const about = screen.getByText(/About/i);
+        user.click(about);
+        expect(about).toBeInTheDocument();
      })
  })
