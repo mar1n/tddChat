e39ba096b14db77a7fd463d11a6b257dd5540593
mar1n <30738216+mar1n@users.noreply.github.com>
2022-08-06 23:29:28 +0100

    bulding shallow rendere finish

--- front/src/tests/shallowHelpers.js
+++ front/src/tests/shallowHelpers.js
@@ -1,4 +1,16 @@
 import React from "react";
+import ShallowRenderer from 'react-test-renderer/shallow';
+
+export const id = id => element => element.props && element.props.id === id;
+export const className = className => element => element.props.className === className;
+export const click = element => element.props.onClick();
+export const type = type => element => element.type === type;
+export const elementsMatching = (element, matcherFn) => {
+    if(matcherFn(element)) {
+        return [element]
+    }
+    return  childrenOf(element).reduce((acc, child) => [...acc,...elementsMatching(child, matcherFn)], []);
+};
 
 export const childrenOf = (element) => {
   if (typeof element === "string") {
@@ -17,3 +29,14 @@ export const childrenOf = (element) => {
   }
   return [children];
 };
+
+export const createShallowRenderer = () => {
+    let renderer = new ShallowRenderer();
+
+    return {
+        shallowRender: component => renderer.render(component),
+        child: n => childrenOf(renderer.getRenderOutput())[n],
+        elementsMatching: matcherFn => elementsMatching(renderer.getRenderOutput(), matcherFn),
+        elementMatching: matcherFn => elementsMatching(renderer.getRenderOutput(), matcherFn)[0]
+    }
+}
--- front/src/tests/shallowHelpers.test.js
+++ front/src/tests/shallowHelpers.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { childrenOf } from "./shallowHelpers";
+import { childrenOf, createShallowRenderer, type } from "./shallowHelpers";
 
 describe("childrenOf", () => {
   test("return no children", () => {
@@ -18,10 +18,76 @@ describe("childrenOf", () => {
   test("return string", () => {
     expect(childrenOf(<div>Text</div>)).toEqual(["Text"]);
   });
-  test('returns no children for text', () => { 
-    expect(childrenOf('text')).toEqual([]);
-   })
-   test('returns array of children for elements with one child', () => { 
-        expect(childrenOf(<div><p>A</p></div>)).toEqual([<p>A</p>])
-    })
+  test("returns no children for text", () => {
+    expect(childrenOf("text")).toEqual([]);
+  });
+  test("returns array of children for elements with one child", () => {
+    expect(
+      childrenOf(
+        <div>
+          <p>A</p>
+        </div>
+      )
+    ).toEqual([<p>A</p>]);
+  });
 });
+
+const TestComponent = ({ children }) => <>{children}</>;
+
+describe("child", () => {
+  let shallowRender, child;
+  beforeEach(() => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    ({ shallowRender, child } = createShallowRenderer());
+  });
+  test("returns undefined if the child does not exist", () => {
+    shallowRender(<TestComponent />);
+    expect(child(0)).not.toBeDefined();
+  });
+  test("returns child of rendered element", () => {
+    shallowRender(
+      <TestComponent>
+        <p>A</p>
+        <p>B</p>
+      </TestComponent>
+    );
+    expect(child(1)).toEqual(<p>B</p>);
+  });
+});
+
+describe("elementsMatching", () => {
+  let shallowRender, elementsMatching;
+
+  beforeEach(() => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    ({ shallowRender, elementsMatching } = createShallowRenderer());
+  });
+
+  test("finds multiple direct children", () => {
+    shallowRender(
+      <TestComponent>
+        <p>A</p>
+        <p>B</p>
+      </TestComponent>
+    );
+    expect(elementsMatching(type("p"))).toEqual([<p>A</p>, <p>B</p>]);
+  });
+  test('finds indirect children', () => { 
+    shallowRender(<TestComponent><div><p>A</p></div></TestComponent>);
+    expect(elementsMatching(type('p'))).toEqual([<p>A</p>]);
+   })
+});
+
+describe('elementMatching', () => { 
+    let shallowRender, elementMatching;
+
+    beforeEach(() => {
+        // eslint-disable-next-line testing-library/no-render-in-setup
+        ({shallowRender, elementMatching} = createShallowRenderer());
+    });
+
+    test('finds first direct child', () => { 
+        shallowRender(<TestComponent><p>A</p><p>B</p></TestComponent>);
+        expect(elementMatching(type('p'))).toEqual(<p>A</p>)
+     })
+ })
