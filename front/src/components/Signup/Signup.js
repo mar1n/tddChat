db1ee91eeec58cfdddc2cba24087f61b2dd577fa
mar1n <30738216+mar1n@users.noreply.github.com>
2022-08-10 16:38:46 +0100

    test Form, field, label, value exist, fn excute

--- front/src/components/Signup/Signup.js
+++ front/src/components/Signup/Signup.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Layout from "../Layout/Layout";
-const Signup = () => {
+const Signup = ({ firstName, onSubmit}) => {
+  const customer = {firstName};
   return (
     <Layout>
       <div>
-        Signup Page <form aria-label="signup form"></form>
+        Signup Page{" "}
+        <form aria-label='signup form' onSubmit={() => onSubmit(customer)}>
+          <label htmlFor="">First Name</label>
+          <input type='text' placeholder="firstName" value={firstName} />
+        </form>
       </div>
     </Layout>
   );
--- front/src/tests/Signup.test.js
+++ front/src/tests/Signup.test.js
@@ -1,12 +1,49 @@
 import React from "react";
 import Signup from "../components/Signup/Signup";
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom"
+import { MemoryRouter } from "react-router-dom";
+import ReactTestUtils from 'react-dom/test-utils';
+
 describe("Signup", () => {
   test("renders a form", () => {
     render(
-   <MemoryRouter><Signup /></MemoryRouter> );
+      <MemoryRouter>
+        <Signup />
+      </MemoryRouter>
+    );
     const form = screen.getByRole("form", { name: /signup form/i });
     expect(form).toBeInTheDocument();
   });
+  test("renders the first name field as text box", () => {
+    render(
+      <MemoryRouter>
+        <Signup />
+      </MemoryRouter>
+    );
+    const field = screen.getByPlaceholderText("firstName");
+    expect(field).not.toBeNull();
+    expect(field.tagName).toEqual("INPUT");
+    expect(field.type).toEqual("text");
+  });
+  test('includes the existing value for the firstName', () => { 
+    render(
+        <MemoryRouter>
+          <Signup firstName="Szymon" />
+        </MemoryRouter>
+      );
+      const field = screen.getByPlaceholderText("firstName");
+      expect(field.value).toEqual("Szymon")
+   })
+   test('renders a label for the first name field', () => {
+    render(<MemoryRouter><Signup /></MemoryRouter>);
+    const label = screen.getByText("First Name");
+    expect(label.tagName).toEqual("LABEL");
+    expect(label).toBeInTheDocument();
+   });
+   test('save existing first name when submitted', async () => {
+        expect.hasAssertions();
+        render(<MemoryRouter><Signup firstName="Szymon" onSubmit={({firstName}) => expect(firstName).toEqual('Szymon')} /></MemoryRouter>);
+        const form = screen.getByRole("form", {name: /signup form/i})
+        await ReactTestUtils.Simulate.submit(form);
+   });
 });
